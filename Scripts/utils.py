ba40a9dd80cc02568c24761a9ce4e12ff349f990
odevincenti <odevincenti@itba.edu.ar>
2023-04-02 00:42:57 -0300

    Agrego funciones para normalizar valores

--- Scripts/utils.py
+++ Scripts/utils.py
@@ -12,6 +12,24 @@ def to_unit(num, sf=3):
 	"""
 	units = ['', 'k', 'M', 'G', 'T', 'p', 'n', 'u', 'm']
 	e_count = 0
+	value, e_count = get_exp(num)
+
+	if e_count > 4 or e_count < -4:
+		return '{:.{}g}'.format(num, sf-1).rstrip('0').rstrip('.')
+	else:
+		return np.format_float_positional(value, precision=sf, unique=True, fractional=False, trim='-') + units[e_count]
+	
+def get_exp(num):
+	"""Normalize a number to its engineering notation.
+
+	Args:
+		num (float): number to be normalized.
+
+	Returns:
+		float: normalized number.
+		int: exponent of 1000 that num was divided by.
+	"""
+	e_count = 0
 	value = num
 	while e_count <= 4 and e_count >= -4:
 		if 1 <= value < 1E3:
@@ -22,9 +40,17 @@ def to_unit(num, sf=3):
 		if value < 1:
 			value *= 1E3
 			e_count -= 1
-	
-	if e_count > 4 or e_count < -4:
-		return '{:.{}g}'.format(num, sf-1).rstrip('0').rstrip('.')
+	return value, e_count
+
+def get_unit_prefix(num):
+	prefix = ['', 'k', 'M', 'G', 'T', 'p', 'n', 'u', 'm']
+	x, e_count = get_exp(num)
+
+	if e_count > 4:
+		unit_prefix = prefix[4]
+	elif e_count < -4:
+		unit_prefix = prefix[4]
 	else:
-		return np.format_float_positional(value, precision=sf, unique=True, fractional=False, trim='-') + units[e_count]
-	
+		unit_prefix = prefix[e_count]
+
+	return unit_prefix
