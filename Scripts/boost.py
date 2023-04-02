4ef355f54b94ca339e095dec2348222fbd666ec1
odevincenti <odevincenti@itba.edu.ar>
2023-04-02 02:12:00 -0300

    Agrego curvas switch y diodo

--- Scripts/boost.py
+++ Scripts/boost.py
@@ -24,6 +24,38 @@ def DCM_IL_off(Vd, Vo, L, D, Ts, Io, t):
         IL = 0
     return IL
 
+def get_VL(Vd, Vo, L, Ts, Io, t):
+    """Obtain the value of the inductor voltage in a Boost converter in an instant t
+
+    Args:
+        Vd (float)
+        Vo (float)
+        L (float)
+        Ts (float)
+        Io (float)
+        t (float): time passed since the Boost converter was turned on
+
+    Returns:
+        float: VL
+    """
+    D = 1 - Vd/Vo
+    Iob = Vo*Ts*((1-D)**2)*D / (2*L)
+    if Io >= Iob:       # If CCM
+        if (t/Ts) % 1 < D:
+            VL = Vd
+        else:
+            VL = -(Vo - Vd)
+    else:
+        D = np.sqrt(( 2*L*Io / (Vd*Ts)) * (Vo/Vd - 1))
+        Delta_1 = 2*L*Io / (Vd*D*Ts)
+        if (t/Ts) % 1 < D:
+            VL = Vd
+        elif (t/Ts) % 1 < D + Delta_1:
+            VL = -(Vo - Vd)
+        else:
+            VL = 0
+    return VL
+
 def get_IL(Vd, Vo, L, Ts, Io, t):
     """Obtain the value of the inductor current in a Boost converter in an instant t
 
@@ -53,25 +85,127 @@ def get_IL(Vd, Vo, L, Ts, Io, t):
             IL = DCM_IL_off(Vd, Vo, L, D, Ts, Io, (t%Ts))
     return IL
             
-def get_VL(Vd, Vo, L, Ts, Io, t):
+def get_Vsw(Vd, Vo, L, Ts, Io, t):
+    """Obtain the value of the switch voltage in a Boost converter in an instant t
+
+    Args:
+        Vd (float)
+        Vo (float)
+        L (float)
+        Ts (float)
+        Io (float)
+        t (float): time passed since the Boost converter was turned on
+
+    Returns:
+        float: Vsw
+    """
     D = 1 - Vd/Vo
     Iob = Vo*Ts*((1-D)**2)*D / (2*L)
     if Io >= Iob:       # If CCM
         if (t/Ts) % 1 < D:
-            VL = Vd
+            Vsw = 0
         else:
-            VL = -(Vo - Vd)
+            Vsw = Vo
     else:
         D = np.sqrt(( 2*L*Io / (Vd*Ts)) * (Vo/Vd - 1))
         Delta_1 = 2*L*Io / (Vd*D*Ts)
         if (t/Ts) % 1 < D:
-            VL = Vd
+            Vsw = 0
         elif (t/Ts) % 1 < D + Delta_1:
-            VL = -(Vo - Vd)
+            Vsw = Vo
         else:
-            VL = 0
-    return VL
+            Vsw = Vd
+    return Vsw
 
+def get_Isw(Vd, Vo, L, Ts, Io, t):
+    """Obtain the value of the switch current in a Boost converter in an instant t
+
+    Args:
+        Vd (float)
+        Vo (float)
+        L (float)
+        Ts (float)
+        Io (float)
+        t (float): time passed since the Boost converter was turned on
+
+    Returns:
+        float: Isw
+    """
+    D = 1 - Vd/Vo
+    Iob = Vo*Ts*((1-D)**2)*D / (2*L)
+    if Io >= Iob:       # If CCM
+        if (t/Ts) % 1 < D:
+            Isw = CCM_IL_on(Vd, L, D, Ts, Io, (t%Ts))
+        else:
+            Isw = 0
+    else:
+        D = np.sqrt(( 2*L*Io / (Vd*Ts)) * (Vo/Vd - 1))
+        if (t/Ts) % 1 < D:
+            Isw = DCM_IL_on(Vd, L, D, Ts, (t%Ts))
+        else:
+            Isw = 0
+    return Isw
+
+def get_VD(Vd, Vo, L, Ts, Io, t):
+    """Obtain the value of the diode voltage in a Boost converter in an instant t
+
+    Args:
+        Vd (float)
+        Vo (float)
+        L (float)
+        Ts (float)
+        Io (float)
+        t (float): time passed since the Boost converter was turned on
+
+    Returns:
+        float: VD
+    """
+    D = 1 - Vd/Vo
+    Iob = Vo*Ts*((1-D)**2)*D / (2*L)
+    if Io >= Iob:       # If CCM
+        if (t/Ts) % 1 < D:
+            Vsw = Vo
+        else:
+            Vsw = 0
+    else:
+        D = np.sqrt(( 2*L*Io / (Vd*Ts)) * (Vo/Vd - 1))
+        Delta_1 = 2*L*Io / (Vd*D*Ts)
+        if (t/Ts) % 1 < D:
+            Vsw = Vo
+        elif (t/Ts) % 1 < D + Delta_1:
+            Vsw = 0
+        else:
+            Vsw = Vo - Vd
+    return Vsw
+
+def get_ID(Vd, Vo, L, Ts, Io, t):
+    """Obtain the value of the diode current in a Boost converter in an instant t
+
+    Args:
+        Vd (float)
+        Vo (float)
+        L (float)
+        Ts (float)
+        Io (float)
+        t (float): time passed since the Boost converter was turned on
+
+    Returns:
+        float: ID
+    """
+    D = 1 - Vd/Vo
+    Iob = Vo*Ts*((1-D)**2)*D / (2*L)
+    if Io >= Iob:       # If CCM
+        if (t/Ts) % 1 < D:
+            IL = 0
+        else:
+            IL = CCM_IL_off(Vd, Vo, L, D, Ts, Io, (t%Ts))
+    else:
+        D = np.sqrt(( 2*L*Io / (Vd*Ts)) * (Vo/Vd - 1))
+        if (t/Ts) % 1 < D:
+            IL = 0
+        else:
+            IL = DCM_IL_off(Vd, Vo, L, D, Ts, Io, (t%Ts))
+    return IL
 
 def CCM_get_Delta_Q(Vd, Vo, D, Ts, L, Io):
     """Calculate the capacitor for a continuous conduction mode boost converter
